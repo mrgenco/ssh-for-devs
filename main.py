81ec0791894850026e645d437afcc9fd2dc2dc4c
mrgenco <mrgenco@gmail.com>
2023-09-08 09:11:09 +0300

    prompt toolkit added

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-import click
-import paramiko
+import sqlite3
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.history import FileHistory
 
-@click.command()
-@click.option('--host', prompt='Enter the hostname', help='Remote host to connect to')
-@click.option('--username', prompt='Enter your username', help='Your SSH username')
-@click.option('--password', prompt='Enter your password', hide_input=True, help='Your SSH password')
-def cli(host, username, password):
-    """This is a simple SSH client application designed for developers"""
-    try:
-        # Create an SSH client
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Create a SQLite database (or connect to an existing one)
+conn = sqlite3.connect('connections.db')
+cursor = conn.cursor()
 
-        # Connect to the remote server
-        ssh.connect(host, username=username, password=password)
+# Create a table to store connection details
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS connections (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        host TEXT,
+        username TEXT,
+        password TEXT
+    )
+''')
+conn.commit()
 
-        while True:
-            command = input(f'{username}@{host}$ ')
-            if command.lower() == 'exit':
-                break
+def add_connection():
+    name = prompt("Connection Name: ")
+    host = prompt("Host: ")
+    username = prompt("Username: ")
+    password = prompt("Password: ", is_password=True)
 
-            # Execute the command
-            stdin, stdout, stderr = ssh.exec_command(command)
-            output = stdout.read().decode('utf-8')
-            click.echo(output)
+    cursor.execute("INSERT INTO connections (name, host, username, password) VALUES (?, ?, ?, ?)", (name, host, username, password))
+    conn.commit()
+    print("Connection added successfully.")
 
-    except paramiko.AuthenticationException:
-        print("Authentication failed, please check your credentials.")
-    except paramiko.SSHException as e:
-        print("Unable to establish SSH connection:", str(e))
-    finally:
-        ssh.close()
+def edit_connection():
+    # Implement editing logic here
+    pass
+
+def show_connections():
+    cursor.execute("SELECT id, name, host FROM connections")
+    connections = cursor.fetchall()
+
+    connection_completer = WordCompleter([f"{conn[0]}: {conn[1]} ({conn[2]})" for conn in connections])
+
+    connection_id = prompt("Select a connection to connect to: ", completer=connection_completer)
+    # Implement connection logic here
+    pass
+
+def main():
+    while True:
+        option = prompt("Choose an option (add, edit, show, exit): ")
+
+        if option.lower() == 'add':
+            add_connection()
+        elif option.lower() == 'edit':
+            edit_connection()
+        elif option.lower() == 'show':
+            show_connections()
+        elif option.lower() == 'exit':
+            break
+        else:
+            print("Invalid option. Please choose from 'add', 'edit', 'show', or 'exit'.")
 
 if __name__ == '__main__':
-    cli()
+    main()
